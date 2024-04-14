2575ec350d3e4b0194b2adad2ed4a7e3b8ac54ea
Roland Bali <roland@spagettikod.se>
2024-04-14 13:28:12 +0200

    fixes

--- goreload.sh
+++ goreload.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 PID=
+DATE=$(date "+%Y-%m-%d %H:%M:%S")
+echo "$DATE Starting up..."
 while true; do
     go run . &
     PID=$!
+    DATE=$(date "+%Y-%m-%d %H:%M:%S")
+    echo "$DATE Running..."
     inotifywait --event modify -qq --recursive /app --include '.*\.go'
     DATE=$(date "+%Y-%m-%d %H:%M:%S")
     echo "$DATE Reloading"
