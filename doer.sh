81b34a29b0bae4d1deb55d4401c5c10d0fcd1ff3
Roland Bali <roland@spagettikod.se>
2024-06-12 16:06:11 +0200

    kill sub processes

--- doer.sh
+++ doer.sh
@@ -1,3 +1,12 @@
-#!/bin/sh
+#!/bin/bash
+
+# cleanup() {
+#     echo "Script terminated. Killing all processes."
+#     pkill -TERM -P $$
+#     exit 1
+# }
+
+# Trap the termination signal
+# trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
 go run .
--- goreload.sh
+++ goreload.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
-PID=
-DATE=$(date "+%Y-%m-%d %H:%M:%S")
-echo "$DATE Starting up..."
+#!/bin/bash
+
+# Function to recursively kill child processes
+function kill_tree {
+    local parent_pid=$1
+    local child_pids=$(pgrep -P $parent_pid)
+
+    for pid in $child_pids; do
+        kill_tree $pid
+    done
+
+    kill $parent_pid
+}
+
+function now {
+    echo "$(date "+%Y-%m-%d %H:%M:%S")"
+}
+
+printf "%s Starting goreload...\n" "$(now)"
+exit 1
 while true; do
+    printf "%s Running doer.sh..." "$(now)"
     /doer.sh &
-    PID=$!
-    DATE=$(date "+%Y-%m-%d %H:%M:%S")
-    echo "$DATE Running..."
     inotifywait --event modify -qq --recursive /app --include '.*\.go'
-    DATE=$(date "+%Y-%m-%d %H:%M:%S")
-    echo "$DATE Reloading"
-    pkill -TERM -P $PID
+    printf "%s Stopping doer.sh..." "$(now)"
+    kill_tree $(pgrep -f doer.sh)
 done
